1f0e3cfbfb8af9a3f1c6e6fd3d0945ea9ae1c69b
Jaiden Chicote <jedezzio@gmail.com>
2020-04-11 00:39:17 +1000

    User Story 1: As a player, I want each round to start with me and the opponent at out respective positions
    Whats new!!:
    - Game Manager prefab (skeleton)
    - Player Prefabs (with seperate input controls
    
    Important Notes:
    - Include two spawn positions in your scene for the prefabs to spawn.

---
+++ Assets/Scripts/Games_Level/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    //Test temporary variables
+    [HideInInspector] public int scoreP1;
+    [HideInInspector] public int setWinsP1;
+
+    [HideInInspector] public int scoreP2;
+    [HideInInspector] public int setWinsP2;
+
+
+    //Additional Temporary Variables
+    //Make Sure to insert these variables
+    public Transform nearestRespawn; //This can be stored into a scriptable object
+    public Transform farthestRespawn;
+
+    public GameObject player1Prefab;
+    public GameObject player2Prefab;
+
+    [Header("Game Events")]
+    public UnityEvent OnLevelStart;
+    public UnityEvent OnPlayerScore;
+    public UnityEvent OnRoundEnd;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player1 = Instantiate(player1Prefab, nearestRespawn.position, nearestRespawn.rotation) as GameObject;
+        PlayerController player1Controller = player1.GetComponent<PlayerController>();
+        player1Controller.playerSelection = PlayerController.PlayerSelection.Player1;
+
+        GameObject player2 = Instantiate(player2Prefab, farthestRespawn.position, farthestRespawn.rotation) as GameObject;
+        PlayerController player2Controller = player1.GetComponent<PlayerController>();
+        player2Controller.playerSelection = PlayerController.PlayerSelection.Player2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowMainMenu()
+    {
+
+    }
+
+    void LoadHud()
+    {
+
+    }
+
+    void ResetMatch()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerSelection
+    {
+        Player1,
+        Player2
+    }
+
+    public PlayerSelection playerSelection;
+    public float baseSpeed = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        MovementController();
+    }
+
+    void MovementController()
+    {
+        switch (playerSelection)
+        {
+            case PlayerSelection.Player1:
+                WASDMovenet();
+                break;
+            case PlayerSelection.Player2:
+                KeyMovement();
+                break;
+        }
+    }
+
+    //Temporary Controller
+    // Update is called once per frame
+
+    //Key Movement
+    void KeyMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            float movementX = baseSpeed * -1;
+            transform.position += transform.right * movementX * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            float movementX = baseSpeed * 1;
+            transform.position += transform.right * movementX * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            float movementY = baseSpeed * 1;
+            transform.position += transform.forward * movementY * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            float movementY = baseSpeed * -1;
+            transform.position += transform.forward * movementY * Time.fixedDeltaTime;
+        }
+    }
+
+
+    //WASD Movement
+    void WASDMovenet()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            float movementX = baseSpeed * -1;
+            transform.position += transform.right * movementX * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            float movementX = baseSpeed * 1;
+            transform.position += transform.right * movementX * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            float movementY = baseSpeed * 1;
+            transform.position += transform.forward * movementY * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            float movementY = baseSpeed * -1;
+            transform.position += transform.forward * movementY * Time.fixedDeltaTime;
+        }
+
+    }
+}
