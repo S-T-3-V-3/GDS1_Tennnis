7ecdf8ea89973154fa73460bc91e47e13b118b20
Kyle Hammer <kylethomashammer@gmail.com>
2020-04-12 16:26:30 +1000

    User Story 8: As a player, I want to hear a sound effect when the projectile bounces so that I know if the projectile has bounced once.

---
+++ Assets/Scripts/Ball Physics/Ball_Collisions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Collisions : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            audioSource.Play();
+        }
+    }
+}
