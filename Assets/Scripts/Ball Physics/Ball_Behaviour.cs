dde30818223cdb9ecaa1f99fa523db0aa5d6d79b
Kyle Hammer <kylethomashammer@gmail.com>
2020-04-12 16:02:33 +1000

    User Story 7: As a player, I want to be able to determine the height of the projectile with its position and a shadow so that I know if it is close to bouncing.

---
+++ Assets/Scripts/Ball Physics/Ball_Behaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Behaviour : MonoBehaviour
+{
+    private Rigidbody ballRigidbody;
+
+    //ballAngle Inspector Test Case: 0x, 400y, 800z
+    public Vector3 ballForces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            SendUpwards();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            SendDownwards();
+        }
+    }
+
+    private void SendUpwards()
+    {
+        ballRigidbody.velocity = Vector3.zero;
+        ballRigidbody.AddForce(ballForces);
+    }
+
+    private void SendDownwards()
+    {
+        ballRigidbody.velocity = Vector3.zero;
+        ballRigidbody.AddForce(new Vector3(ballForces.x, ballForces.y, -ballForces.z));
+    }
+}
